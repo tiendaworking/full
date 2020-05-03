7b47825d2e426301a8ffaa54b6d585d61282280e
belem-mtz <64752677+belem-mtz@users.noreply.github.com>
2020-05-03 16:58:23 -0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,63 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Documento sin título</title>
+</head>
+
+<body>
+	
+	
+	
+	
+	
+	
+	
+<div class="header">
+Header
+<div id="logotipo">
+Logoripo
+</div>
+<div id="menu">
+Menu	
+</div>	
+<div id="buscador">
+Buscador 
+</div>	
+</div>	
+	
+	
+	
+<div class="slider">
+Slider
+</div>		
+	
+	
+	
+<div class="categorias">
+Categorias
+</div>		
+	
+	
+	
+	
+<div class="especiales">
+Especiales
+</div>		
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
+	
+</body>
+<link rel="stylesheet" type="text/css" href="/desing/css/estilos.css">
+</html>
